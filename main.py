1f2fd4fb6c3e1d58c692a07f46a00c358e12c822
gumshuda101 <73026587+gumshuda101@users.noreply.github.com>
2023-01-02 08:53:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def hello():
+  print("hello!! hema") # used tab for the space
+  
+def bye():
+  print("bye!! hema") # used2 spaces here
+  
+print(hello())
+
+#messy code
+#linter is not gonna like this at all 
